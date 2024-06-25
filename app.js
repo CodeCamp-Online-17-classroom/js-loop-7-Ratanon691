4facf4df66077da12814dd82f9d7861cace64b8e
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-25 11:00:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// write code here
+function findPrimeNumbers(start, end) {
+    function isPrime(num) {
+        if (num < 2) return false 
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false
+        }
+        return true
+    }
+
+    const primeNumbers = [];
+    for (let i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            primeNumbers.push(i)
+        }
+    }
+    return primeNumbers
+}
+
+
+console.log(findPrimeNumbers(10, 30)); 
+console.log(findPrimeNumbers(1, 10)); 
+console.log(findPrimeNumbers(15, 17)); 
